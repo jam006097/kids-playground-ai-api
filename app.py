6053168124d736af61a4533c79e42786dcc8c765
jam <jam006097@gmail.com>
2025-09-04 21:04:37 +0000

    ユーザー名を元にもどす

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from ai_api.main import iface  # noqa: E402
 # 環境変数から認証情報を取得
 # gradioのauthの仕様上、ユーザ名は必須だが、今回はパスワードのみ使用する
 # そのため、ユーザ名は固定
-auth_user = "kina"
+auth_user = "gemini"
 auth_password = os.getenv("GRADIO_PASSWORD")
 
 # 認証情報が両方設定されている場合のみ、認証を有効にする
